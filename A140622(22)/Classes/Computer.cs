bbd6809bf5749c535bdd42b4124b1a638d3f9785
FATIH <FATIH@ASUS>
2022-06-14 20:03:28 +0300

    Add project files.

---
+++ A140622(22)/Classes/Computer.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A140622_22_.Classes
+{
+    public class Computer
+    {
+        public Computer()
+        {
+
+
+        }
+        public Computer(int id, string brand, double cpuClockRate)
+        {
+            this.ID = id;
+            this.Brand = brand;
+            this.CPUClockRate = cpuClockRate;
+        }
+        ~Computer() //Tilda İşaretiyle yazılır. (~)
+        {
+
+
+        }
+        private int price;
+        private string configurationSettings;
+
+        public int PriceForCustomer
+        {
+            get { return price; }
+            set
+            {
+                if (value < 0)
+                {
+                    price = 0;
+                }
+                else if (value > 25000)
+                {
+                    price = 2000;
+                }
+                else
+                {
+                    price = value;
+                }
+            }
+        }
+
+        public string ConfigurationSettingsForCustomer
+        {
+            get
+            {
+
+                return "Settings: " + configurationSettings;
+            }
+            set
+            {
+                if (value.Trim() == "")
+                {
+                    configurationSettings = "Default Cofiguration";
+                }
+                else
+                {
+                    configurationSettings = value;
+                }
+
+            }
+        }
+
+
+
+        private int ID { get; set; }
+
+        private string Brand { get; set; }
+
+        private double CPUClockRate { get; set; }
+
+        public void Boot()
+        {
+
+
+
+        }
+        public void ShutDown()
+        {
+
+
+
+        }
+
+
+
+
+
+
+
+    }
+
+
+}
---
+++ A140622(22)/Program.cs
@@ -0,0 +1,29 @@
+﻿using A140622_22_.Classes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A140622_22_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Computer computerA = new Computer(11941, "Lenovo", 3.25);
+            computerA.PriceForCustomer = 25042;
+            computerA.ConfigurationSettingsForCustomer = "cmd_adegsdf";
+
+            Computer computerB = new Computer(12041, "HP", 2.95);
+            computerB.ConfigurationSettingsForCustomer = "";
+            computerB.PriceForCustomer = 25015;
+
+            Computer computerC = new Computer(1542, "Dell", 2.50);
+            computerC.ConfigurationSettingsForCustomer = "";
+            computerC.PriceForCustomer = 450;
+
+
+        }
+    }
+}
